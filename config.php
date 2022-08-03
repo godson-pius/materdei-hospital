5852fdc33ce90a959cbddc0ecf7d3b612e7e636a
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-08-03 14:29:23 +0100

    Updated Config File

--- config.php
+++ config.php
@@ -8,3 +8,4 @@ $hospital_num = "09022114910";
 $num_design = "+(234)90 2211 4910";
 $venue = "15 Umukwa street, by ESBS b/stop, Independence layout, Enugu";
 $banner = "background-image: url(assets/img/inner-banner/inner-banner-1.jpg)";
+$developer = "World Brain Technology Ltd";
