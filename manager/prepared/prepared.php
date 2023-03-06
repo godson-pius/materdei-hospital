5ac8929e2beb2e6e0fb99a9f1dfab2e01ac60eca
Godson Pius <godsonazubuike15@gmail.com>
2023-03-06 10:20:13 +0100

    changed database connection parameters

--- manager/prepared/prepared.php
+++ manager/prepared/prepared.php
@@ -3,5 +3,5 @@ session_start();
 
 require_once 'lib.php';
 //= Set up automatically been called
-//SETUP("localhost", "matehaww_materdeiuser", "100%materdei", "matehaww_materdei");
- SETUP("localhost", "root", "", "materdei");
+SETUP("localhost", "matehaww_materdeiuser", "100%materdei", "matehaww_materdei");
+// SETUP("localhost", "root", "", "materdei");
